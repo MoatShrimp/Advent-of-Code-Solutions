4a4725ca3cb0f04208266fc039e0137d6d198c05
Mathias Viklund <mathias.viklund@gmail.com>
2024-02-19 16:30:54 +0100

    feat: add aggregation method

--- csharp/2023/AdventOfCode/Day1.cs
+++ csharp/2023/AdventOfCode/Day1.cs
@@ -1,4 +1,4 @@
-
+﻿
 namespace AdventOfCode;
 
 public class Day1
@@ -6,5 +6,13 @@ public class Day1
   private static int GetFirstAndLastNumericInString(string str)
   {
     throw new NotImplementedException();
+  }
+  
+  public static int Calibrater(string exampleInput)
+  {
+    return exampleInput.Split('\n')
+      .Aggregate(
+        0,
+        (curr, next) => curr + GetFirstAndLastNumericInString(next));
   }
 }
