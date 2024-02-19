cc9b0b5406c9a647e934ab914023a20971003d3b
Mathias Viklund <mathias.viklund@gmail.com>
2024-02-19 16:41:20 +0100

    feat: solved Day1.Test1

--- csharp/2023/AdventOfCode/Day1.cs
+++ csharp/2023/AdventOfCode/Day1.cs
@@ -5,7 +5,9 @@ public class Day1
 {
   private static int GetFirstAndLastNumericInString(string str)
   {
-    throw new NotImplementedException();
+    var num1 = str.First(c => c is >= '0' and <= '9');
+    var num2 = str.Reverse().First(c => c is >= '0' and <= '9');
+    return int.Parse(new string([num1, num2]));
   }
   
   public static int Calibrater(string exampleInput)
